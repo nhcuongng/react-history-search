4d0f6e1d8acca553698d8de35320621cc51cf2f8
nhcuongng <cuong.nguyenhuu1997@gmail.com>
2020-09-10 00:00:06 +0700

    v0.0.1 done base 2

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,8 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
-import { terser } from "rollup-plugin-terser";
+// import { terser } from "rollup-plugin-terser";
+import autoNamedExports from 'rollup-plugin-auto-named-exports';
 import css from 'rollup-plugin-css-only'
 
 import pkg from './package.json';
@@ -32,10 +33,14 @@ export default {
       exclude: '**/__tests__/**',
       clean: true,
     }),
-    terser(), // minifies generated bundles
+    // terser(), // minifies generated bundles
     commonjs({
       include: ['node_modules/**'],
+      namedExports: {
+        // no need manual custom
+      }
     }),
+    autoNamedExports(),
     css({ output: 'bundle.css' })
   ],
 };
